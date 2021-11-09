faf575860552deeb6b2bf45a0e0569a04ed21e13
hectorg2211 <hector032716@gmail.com>
2021-11-09 11:53:16 -0800

    Changed API route

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:9000",
+  baseURL: "https://heythere-app.herokuapp.com/",
 });
 
 export default instance;
